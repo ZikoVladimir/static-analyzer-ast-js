08c560ef26262a6e0d0b0e8730999ba4bc9544e4
Vladimir Zykov <zykov@haulmont.com>
2020-03-29 20:20:00 +0400

    implemented test no-sync-xhr

--- src/tests/no-blocking-event-loop/utils.ts
+++ src/tests/no-blocking-event-loop/utils.ts
@@ -4,11 +4,19 @@ import traverse from '@babel/traverse';
 export const getLineNumberList = (ast): string[] => {
   const lineNumberList: string[] = [];
 
+  let declaratorList: string[] = [];
+
+  traverse(ast, {
+    VariableDeclaration(path) {
+      declaratorList = [...declaratorList ,...getXhrDeclaratorList(path.node)];
+    }
+  });
+
   traverse(ast, {
     MemberExpression(path) {
       if (type.isIdentifier(path.node.property, { name: 'alert' }) &&
         type.isIdentifier(path.node.object, { name: 'window' }) ||
-        findSyncAjax(path)) {
+        findSyncAjax(path) || findSyncXhr(path, declaratorList)) {
         lineNumberList.push(path.node.loc.start.line);
       }
     },
@@ -16,7 +24,7 @@ export const getLineNumberList = (ast): string[] => {
       if (type.isIdentifier(path.node.callee, { name: 'alert' })) {
         lineNumberList.push(path.node.loc.start.line);
       }
-    }
+    },
   });
 
   return lineNumberList;
@@ -34,4 +42,26 @@ function findAsyncProperty(properties): boolean {
     return type.isIdentifier(property.key, { name: 'async' }) &&
       type.isBooleanLiteral(property.value, { value: false });
   });
+}
+
+// todo: refactor
+
+function getXhrDeclaratorList(node): string[] {
+  return node.declarations
+    .filter(declarator => {
+      return type.isNewExpression(declarator.init) &&
+        type.isIdentifier(declarator.init.callee, { name: 'XMLHttpRequest' })
+    })
+    .map(declarator => declarator.id.name);
+}
+
+function findSyncXhr(path, declaratorList: string[]): boolean {
+  return checkDeclaratorList(declaratorList, path.node.object.name) &&
+    type.isIdentifier(path.node.property, { name: 'open' }) &&
+    path.parent.arguments && path.parent.arguments.length >= 2 &&
+    type.isLiteral(path.parent.arguments[2], { value: false });
+}
+
+function checkDeclaratorList(declaratorList: string[], value: string): boolean {
+  return !!declaratorList.find(declarator => declarator === value);
 }
